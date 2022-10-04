e03651c1a9241f852ae39e48973294595be30626
Frank Kachinga <frank.kachinga@gmail.com>
2022-10-04 20:38:09 +0300

    initial setup

---
+++ src/test/java/com/kachinga/bookmarker/BookmarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kachinga.bookmarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
